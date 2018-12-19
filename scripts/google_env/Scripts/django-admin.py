d0c47cb9749845b316e05b843fa28c11526e4080
Hao Wu <wowwh@umich.edu>
2018-12-19 04:39:52 -0500

    git rm env

--- scripts/google_env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\wuhao\si664\si664_finalproject\scripts\google_env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
